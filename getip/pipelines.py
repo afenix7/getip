91323c4650f89e03fa23f00c4ff61ece8aa8a65a
沈洋 <sy109491@alibaba-inc.com>
2018-12-04 21:33:51 +0800

    crawl spider

--- getip/pipelines.py
+++ getip/pipelines.py
@@ -4,6 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
 
 class GetipPipeline(object):
@@ -11,14 +12,14 @@ class GetipPipeline(object):
         self.file = open("list.txt", "a")
 
     def process_item(self, item, spider):
-        if item['type'] == 'HTTP' and '天' in str(item['time']):
+        if str(item['type']) == 'HTTP' and '天' in str(item['time']):
             item_string = 'http://' + str(item['host']) + ':' + str(item['port'])
             self.file.write(item_string)
             self.file.write('\n')
             print(item_string)
             return item
         else:
-            return None
+            raise DropItem('drop item: %s' % item)
 
     def close_spider(self, spider):
         self.file.close()
--- getip/spiders/xici2.py
+++ getip/spiders/xici2.py
@@ -13,11 +13,12 @@ class Xici2Spider(CrawlSpider):
 
     rules = (
         # 分页
-        Rule(LinkExtractor(allow=r'http://www.xicidaili.com/nn/\d+/$'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'http://www.xicidaili.com/nn/(20|1[0-9]|[1-9])$'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
         # 参考 https://www.cnblogs.com/qlshine/p/5926103.html
+        print(response.url)
         table = response.xpath('//table[@id="ip_list"]')[0]
         trs = table.xpath('//tr')[1:]  # 去掉标题行
         items = []
