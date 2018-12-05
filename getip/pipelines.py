b8cb693b5b8ec1f60494fa15ce054db82968de8c
aphx <aphx1988@qq.com>
2018-12-05 16:27:45 +0800

    211212

--- getip/pipelines.py
+++ getip/pipelines.py
@@ -5,21 +5,71 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exceptions import DropItem
+from bs4 import BeautifulSoup
+import urllib
+import requests
+import socket
+import traceback
+import sys
+import lxml
+
+"""
+作者：木马音响积木
+链接：https://www.jianshu.com/p/c481feaf44fb
+來源：简书
+简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+"""
 
 
 class GetipPipeline(object):
+    validate_url = "http://ip.chinaz.com/getip.aspx"
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
+    }
+
     def __init__(self):
-        self.file = open("list.txt", "a")
+        self.file = open("iplist.txt", "a")
+        self.sfile = open("siplist.txt", "a")
+
+    def process_ip(self, ip_type, host, port):
+        return 'https://' + host + ':' + port if ip_type == 'HTTPS' else 'http://' + host + ':' + port
+
+    def validate_ip(self, proxy):
+        try:
+            socket.setdefaulttimeout(3)
+            proxy_temp = {"http": proxy}
+            wb_data = requests.get(self.validate_url, headers=self.headers, proxies=proxy_temp)
+            soup = BeautifulSoup(wb_data.text, 'lxml')
+            print(soup)
+            print(proxy)
+            return True
+        except:
+            return False
+
+    def write_ip(self, ip_type, item_string):
+        try:
+            if ip_type == 'HTTPS':
+                self.sfile.write(item_string)
+                self.sfile.write('\n')
+            else:
+                self.file.write(item_string)
+                self.file.write('\n')
+            print("增加IP: " + item_string)
+        except:
+            print('写入文件时发生错误')
 
     def process_item(self, item, spider):
-        if str(item['type']) == 'HTTP' and '天' in str(item['time']):
-            item_string = 'http://' + str(item['host']) + ':' + str(item['port'])
-            self.file.write(item_string)
-            self.file.write('\n')
-            print(item_string)
+        if '天' in str(item['time']):
+            item_string = self.process_ip(str(item['type']), str(item['host']), str(item['port']))
+            if self.validate_ip(item_string):
+                self.write_ip(str(item['type']), item_string)
+            else:
+                print("\033[0;31m验证失败: %s\033[0m" % item_string)
             return item
         else:
             raise DropItem('drop item: %s' % item)
 
     def close_spider(self, spider):
         self.file.close()
+        self.sfile.close()
